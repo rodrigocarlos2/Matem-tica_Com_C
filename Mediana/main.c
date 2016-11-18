7744efc66277a18580ab1216e26320375c756212
Rodrigo Carlos <rodrigo19962010@live.com>
2016-11-18 12:41:32 -0300

    Iniciado projeto Mediana com Rodrigo

---
+++ Mediana/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+// Author: Rodrigo Carlos
+
+void ordenarLista(int vet[], int tam){
+	
+}
+
+int main(int argc, char *argv[]) {
+	
+	
+	
+	return 0;
+}
